36c6fdd3293fa6515959074608f8825552d53674
Opilane TTHK <opilane@tthk.ee>
2021-11-09 15:49:15 +0200

    horoscope

---
+++ Colours/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Colours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda lemmik värv");
+            string värv = Console.ReadLine();
+
+            Console.WriteLine(GetColor(värv));
+
+
+        }
+
+        public static string[] ReadColor()
+        {
+            string FilePath = @"C:\Users\opilane\samples\Color.txt";
+            string[] datafromfile = File.ReadAllLines(FilePath);
+            
+
+            return datafromfile;
+        }
+
+        public static string GetColor(string UserInput)
+        {
+            string[] Color = ReadColor();
+            string todayColor = "";
+            foreach (string elements in Color)
+            {
+                if (elements.Contains(UserInput))
+                {
+                    todayColor = elements;
+                }
+            }
+            return todayColor;
+        }
+    }
+}
---
+++ Horoscope/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda tähtkuju");
+            string tahtkuju = Console.ReadLine();
+
+            Console.WriteLine(GetHoroscope(tahtkuju));
+
+
+        }
+
+        public static string[] ReadHoroscope()
+        {
+            string FilePath = @"C:\Users\opilane\samples\horoscope.txt";
+            string[] datafromfile = File.ReadAllLines(FilePath);
+            Console.WriteLine(datafromfile.Length);
+
+            return datafromfile;
+        }
+
+        public static string GetHoroscope(string UserInput)
+        {
+            string[] horoscope = ReadHoroscope();
+            string todayHoroscope = "";
+            foreach(string elements in horoscope)
+            {
+                if (elements.Contains(UserInput))
+                {
+                    todayHoroscope = elements;
+                }
+            }
+            return todayHoroscope;
+        }
+
+
+    }
+}
